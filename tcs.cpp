42aa86b087efe3b6a728b6670875328de727e987
Sabsterrexx <saabitzubairi@gmail.com>
2021-04-17 14:42:42 -0400

    testing quicksort and changed read fro file in test cases

--- tcs.cpp
+++ tcs.cpp
@@ -3,16 +3,24 @@
 #include "sort.hpp"
 #include "printElements.hpp"
 
+std::vector<int>unsorted;
+
 std::vector<int>input;
+
 std::vector <int> output;
+
+
 
 TEST_CASE("Good to go","[vector]"){
 
-    readFromFile("numbers.txt",input);
+    readFromFile("numbers.txt",unsorted);
     readFromFile("numbers-out.txt",output);
+
+    input = unsorted;
+
     REQUIRE(bubbleSort(input) == output);
 
-    readFromFile("numbers.txt",input);
+    input = unsorted;
     REQUIRE(quickSort(input,0,9) == output);
     
 }
