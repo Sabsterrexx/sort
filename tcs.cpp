23eff41a875d2a015951ca306640bb6256c36de8
Sabsterrexx <saabitzubairi@gmail.com>
2021-04-17 23:15:26 -0400

    first benchmarking test

--- tcs.cpp
+++ tcs.cpp
@@ -19,11 +19,32 @@ TEST_CASE("Good to go","[vector]"){
     input = unsorted;
     REQUIRE(bubbleSort(input) == output);
 
+    CHECK(bubbleSort(input) == output);
+
+    BENCHMARK("Bubble Sort"){
+
+        return bubbleSort(input);
+    };
+
     input = unsorted;
     REQUIRE(quickSort(input,0,9) == output);
     
+    CHECK(quickSort(input,0,9) == output);
+
+    BENCHMARK("Quick Sort"){
+
+        return quickSort(input,0,9);
+    };
 
     input = unsorted;
     REQUIRE(insertionSort(input,10000) == output);
+
+    CHECK(insertionSort(input,10000) == output);
+
+    BENCHMARK("Insertion Sort"){
+
+        return insertionSort(input,10000);
+    };
+
 }
 
